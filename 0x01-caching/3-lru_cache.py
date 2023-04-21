640481ba79dbc9c2f475268331a3236796787317
BirukL137 <buraf13k@gmail.com>
2023-04-21 20:05:22 +0300

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,7 +13,7 @@ class LRUCache(BaseCaching):
     def __init__(self):
         ''' Initializing class attribute. '''
         super().__init__()
-        self.keys = []
+        self.list = []
 
     def put(self, key, item):
         """
@@ -24,12 +24,12 @@ class LRUCache(BaseCaching):
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
+            if key not in self.list:
+                self.list.append(key)
             else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(0)
+                self.list.append(self.list.pop(self.list.index(key)))
+            if len(self.list) > BaseCaching.MAX_ITEMS:
+                discard = self.list.pop(0)
                 del self.cache_data[discard]
                 print('DISCARD: {:s}'.format(discard))
 
@@ -39,6 +39,6 @@ class LRUCache(BaseCaching):
         key, if key is not None and if key exists in dictionary.
         """
         if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
+            self.list.append(self.list.pop(self.list.index(key)))
             return self.cache_data[key]
         return None
