fbea5b9fc61dd0c632459a365d20cae6ff3b7e82
BirukL137 <buraf13k@gmail.com>
2023-04-21 20:02:45 +0300

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-''' LRU Caching: Create a class LRUCache that inherits from BaseCaching
-                 and is a caching system
+'''
+LRU Caching
 '''
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    ''' An LRU Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the least recently used entry to
-        accommodate for the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
-
+    """
+    A class LRUCache that inherits from BaseCaching
+    """
     def __init__(self):
-        ''' Initialize class instance. '''
+        ''' Initializing class attribute. '''
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
-        ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard least recently used entry in cache to accommodate new
-            entry. '''
+        """
+        This function assign the key with a value to the dictionary if
+        the key or item is not None, otherwise return nothing, also checks
+        if the size of the dictionary higher than MAX_ITEM, if it does it'll
+        discard the least recently used item based on LRU algoritm.
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.keys:
@@ -39,8 +34,10 @@ class LRUCache(BaseCaching):
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
+        """
+        This function returns the value in the dictionary that is linked to
+        key, if key is not None and if key exists in dictionary.
+        """
         if key is not None and key in self.cache_data:
             self.keys.append(self.keys.pop(self.keys.index(key)))
             return self.cache_data[key]
