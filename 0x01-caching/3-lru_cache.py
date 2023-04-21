5cdf3072ca7bbd7355e8150be0ba66a465099ae8
BirukL137 <buraf13k@gmail.com>
2023-04-21 14:46:04 +0300

    3. LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+LRU Caching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        """ initializing class attributes """
+        super().__init__()
+        self.list = []
+
+    def put(self, key, item):
+        """
+        This function assign the key with a value to the dictionary if
+        the key or item is not None, otherwise return nothing, also checks
+        if the size of the dictionary higher than MAX_ITEM, if it does it'll
+        discard the least recently used item based on LRU algoritm.
+        """
+        if key is not None and item is not None:
+            if key in self.list:
+                self.list.remove(key)
+                del self.cache_data[key]
+            self.list.append(key)
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard_key = self.list.pop(0)
+            del self.cache_data[discard_key]
+            print(f"DISCARD: {discard_key}")
+
+    def get(self, key):
+        """
+        This function returns the value in the dictionary that is linked to
+        key, if key is not None and if key exists in dictionary.
+        """
+        if key is not None and key in self.cache_data:
+            self.list.append(key)
+            self.list.remove(key)
+            return self.cache_data[key]
+        return None
