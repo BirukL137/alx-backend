d40e198405818637cdd20604551d6433defb0447
BirukL137 <buraf13k@gmail.com>
2023-04-21 20:52:53 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+MRU Caching
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    A class MRUCache that inherits from BaseCaching
+    """
+    def __init__(self):
+        ''' Initializing class attribute. '''
+        super().__init__()
+        self.list = []
+
+    def put(self, key, item):
+        """
+        This function assign the key with a value to the dictionary if
+        the key or item is not None, otherwise return nothing, also checks
+        if the size of the dictionary higher than MAX_ITEM, if it does it'll
+        discard the least recently used item based on LRU algoritm.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.list:
+                self.list.append(key)
+            else:
+                self.list.append(self.list.pop(self.list.index(key)))
+
+            if len(self.list) > BaseCaching.MAX_ITEMS:
+                discard_key = self.list.pop(-2)
+                del self.cache_data[discard_key]
+                print("DISCARD: {:s}".format(discard_key))
+
+    def get(self, key):
+        """
+        This function returns the value in the dictionary that is linked to
+        key, if key is not None and if key exists in dictionary.
+        """
+        if key is not None and key in self.cache_data:
+            self.list.append(self.list.pop(self.list.index(key)))
+            return self.cache_data[key]
+        return None
